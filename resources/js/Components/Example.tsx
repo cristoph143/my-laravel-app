8e84a233a48fc31f7d117e02405aa501bab3952b
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-10-11 23:25:39 +0800

    first commit

---
+++ resources/js/Components/Example.tsx
@@ -0,0 +1,13 @@
+// resources/js/components/Example.tsx
+
+import React from 'react';
+
+function Example() {
+    return (
+        <div>
+            <p>This is a React component in TypeScript.</p>
+        </div>
+    );
+}
+
+export default Example;
